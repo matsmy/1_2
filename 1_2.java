f538019bb333aed2f820d72160beb68adf3aab22
matsmy <101180563+matsmy@users.noreply.github.com>
2022-03-22 10:09:06 +0000

    Initial commit

---
+++ 1_2.java
@@ -0,0 +1,11 @@
+class Main
+  {
+   public static void main(String[] args)
+    {
+      short x = 10;
+      double y = 10.1;
+      long z = 10202023;
+      int q = 1000;
+      System.out.println("short: "+x+"\ndouble: "+y+"\nlong: "+z+"\nint: "+q);
+    }
+  }
